c318bae445226864040e16f88313aa4aea5d30ad
Abner Eduardo Silveira Santos <abner.santos@usp.br>
2018-10-30 14:09:36 -0300

    Player attack, crouch and double jump

--- Anathema/Assets/Scripts/Player/JumpFall.cs
+++ Anathema/Assets/Scripts/Player/JumpFall.cs
@@ -2,64 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpFall : Anathema.Fsm.PlayerState
+namespace Anathema.Player
 {
-	[Tooltip("Force that pulls player down. It's recommended to be higher than the base gravity of the ascension state.")]
-	[SerializeField] float gravity;
-
-	[Tooltip("The distance of the ray that checks for ground, on fall. Recommended to be a little higher than the distance between the center of the player and the ground.")]
-	[SerializeField] float groundCheckDist;
-
-	[Tooltip("Speed in which the player is able to control himself during the descent.")]
-	[SerializeField] float horizontalSpeed;
-
-	[Tooltip("Offset in which the raycast win begin, this affects the height and proximity in which the player needs to be to a platform to snap to it. Recomended to be a little lower than the Ground Check Distance.")]
-	[SerializeField] float raycastOffset;
-
-	// Stores whether or not the player has double jumped
-	private bool hasDoubleJumped;
-
-	public override void Enter() {	}
-
-	/// <summary>
-	/// 	In this class, the FixedUpdate handles the forces responsable for making the player fall and the transitions to other states
-	///  such as when the player reaches the ground or double jumps. Also handles air movement.
-	/// </summary>
-	void FixedUpdate()
+	public class JumpFall : Anathema.Fsm.PlayerState
 	{
-		float HorizontalAxis = Input.GetAxisRaw("Horizontal");
+		[Tooltip("Force that pulls player down. It's recommended to be higher than the base gravity of the ascension state.")]
+		[SerializeField] float gravity;
 
-		// Handles gravity forces pulling the player to the ground
-		rBody.AddForce(Vector2.down * gravity);
+		[Tooltip("The distance of the ray that checks for ground, on fall. Recommended to be a little higher than the distance between the center of the player and the ground.")]
+		[SerializeField] float groundCheckDist;
 
-		// Raycast to check for ground
-		RaycastHit2D rayHit = Physics2D.Raycast((Vector2)transform.position + (Vector2.down * raycastOffset), Vector2.down, groundCheckDist - raycastOffset, LayerMask.GetMask("Ground"));
-		Debug.DrawRay(transform.position, Vector2.down * groundCheckDist, Color.green);
+		[Tooltip("Speed in which the player is able to control himself during the descent.")]
+		[SerializeField] float horizontalSpeed;
 
-		// Transitions states as soon as the player reaches ground
-		if(rayHit)
+		[Tooltip("Offset in which the raycast win begin, this affects the height and proximity in which the player needs to be to a platform to snap to it. Recomended to be a little lower than the Ground Check Distance.")]
+		[SerializeField] float raycastOffset;
+
+		[Tooltip("Whether or not the player has unlocked the skill to double jump.")]
+		[SerializeField] bool canDoubleJump;
+
+		// Stores whether or not the player has double jumped
+		private bool hasDoubleJumped;
+
+		public override void Enter() {	}
+
+		/// <summary>
+		/// 	In this class, the FixedUpdate handles the forces responsable for making the player fall and the transitions to other states
+		///  such as when the player reaches the ground or double jumps. Also handles air movement.
+		/// </summary>
+		void FixedUpdate()
 		{
-			this.transform.position += ((Vector3)rayHit.normal * ((groundCheckDist - raycastOffset) - rayHit.distance));
-			animator.SetBool("IsWalking", true);
-			animator.SetBool("IsFalling", false);
-			fsm.Transition<Walking>();
+			float HorizontalAxis = Input.GetAxisRaw("Horizontal");
+
+			// Handles gravity forces pulling the player to the ground
+			rBody.AddForce(Vector2.down * gravity);
+
+			// Raycast to check for ground
+			RaycastHit2D rayHit = Physics2D.Raycast((Vector2)transform.position + (Vector2.down * raycastOffset), Vector2.down, groundCheckDist - raycastOffset, LayerMask.GetMask("Ground"));
+			Debug.DrawRay(transform.position, Vector2.down * groundCheckDist, Color.green);
+
+			// Transitions states as soon as the player reaches ground
+			if(rayHit)
+			{
+				this.transform.position += ((Vector3)rayHit.normal * ((groundCheckDist - raycastOffset) - rayHit.distance));
+				animator.SetBool("IsWalking", true);
+				animator.SetBool("IsFalling", false);
+				hasDoubleJumped = false;
+				fsm.Transition<Walking>();
+			}
+
+			if(Input.GetKeyDown(KeyCode.Space) && !hasDoubleJumped && canDoubleJump)
+			{
+				animator.SetBool("IsRising", true);
+				animator.SetBool("IsFalling", false);
+				hasDoubleJumped = true;
+				rBody.velocity = new Vector2(rBody.velocity.x, 0f);
+				fsm.Transition<JumpRise>();
+				return;
+			}
+
+			// Handles air movement
+			if(HorizontalAxis == 0f)
+				rBody.velocity = new Vector2(0f, rBody.velocity.y);
+			else if(HorizontalAxis > 0f)
+			{
+				sRenderer.flipX = false;
+				rBody.velocity = new Vector2(horizontalSpeed, rBody.velocity.y);
+			}
+			else
+			{
+				sRenderer.flipX = true;
+				rBody.velocity = new Vector2(-horizontalSpeed, rBody.velocity.y);
+			}
+
 		}
 
-		// Handles air movement
-		if(HorizontalAxis == 0f)
-			rBody.velocity = new Vector2(0f, rBody.velocity.y);
-		else if(HorizontalAxis > 0f)
-		{
-			sRenderer.flipX = false;
-			rBody.velocity = new Vector2(horizontalSpeed, rBody.velocity.y);
-		}
-		else
-		{
-			sRenderer.flipX = true;
-			rBody.velocity = new Vector2(-horizontalSpeed, rBody.velocity.y);
-		}
-
+		public override void Exit() {	}
 	}
-
-	public override void Exit() {	}
 }
--- Anathema/Assets/Scripts/Player/Walking.cs
+++ Anathema/Assets/Scripts/Player/Walking.cs
@@ -2,104 +2,136 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Walking : Anathema.Fsm.PlayerState
+namespace Anathema.Player
 {
-	[Tooltip("Speed in which the player is able to move.")]
-	[SerializeField] float baseSpeed;
-
-	[Tooltip("The EXACT measure between the center of the player and the ground level when he's grounded.")]
-	[SerializeField] float groundRayDist;
-
-	[Tooltip("An added distance that is added to the Ground Ray Distance to detect ground in uneven ground, such as stairs and slopes.")]
-	[Range(0f, 1f)] [SerializeField] float safetyGroundThreshold;
-
-	[Tooltip("The maximum slope angle in which the player can walk on, without falling.")]
-	[SerializeField] float steepSlopeAngle;
-
-	// Stores the result of the raycast
-	private RaycastHit2D rayHit;
-
-	// Stores the vector in which the player's velocity needs to be multiplied by
-	private Vector2 moveDirection;
-
-	public override void Enter() {	}
-
-	/// <summary>
-	/// 	Gets ground information, more specifically the angle of the current, being walked at, tile.
-	/// 	Based on the normal vector of said tile, returns, by parameter, the vector in which the player's velocity, on movement, needs to move at.
-	/// </summary>
-	/// <param name="rayHit"> The result of the raycast. </param>
-	/// <param name="moveDirection"> The resulting vector of the player's movement. </param>
-	void GetGroundInformation(RaycastHit2D rayHit, out Vector2 moveDirection)
+	public class Walking : Anathema.Fsm.PlayerState
 	{
-		float slopeAngle = Vector2.Angle(Vector2.up, rayHit.normal);
-		if(slopeAngle <= steepSlopeAngle)
+		[Tooltip("Speed in which the player is able to move.")]
+		[SerializeField] float baseSpeed;
+
+		[Tooltip("The EXACT measure between the center of the player and the ground level when he's grounded.")]
+		[SerializeField] float groundRayDist;
+
+		[Tooltip("An added distance that is added to the Ground Ray Distance to detect ground in uneven ground, such as stairs and slopes.")]
+		[Range(0f, 1f)] [SerializeField] float safetyGroundThreshold;
+
+		[Tooltip("An offset distance between the center of the player and where the raycast actually begins casting. Has to be lower than groundRayDist.")]
+		[SerializeField] float raycastOffset;
+
+		[Tooltip("The maximum slope angle in which the player can walk on, without falling.")]
+		[SerializeField] float steepSlopeAngle;
+
+		// Stores the result of the raycast
+		private RaycastHit2D rayHit;
+
+		// Stores the vector in which the player's velocity needs to be multiplied by
+		private Vector2 moveDirection;
+
+		public override void Enter() {	}
+
+		/// <summary>
+		/// 	Gets ground information, more specifically the angle of the current, being walked at, tile.
+		/// 	Based on the normal vector of said tile, returns, by parameter, the vector in which the player's velocity, on movement, needs to move at.
+		/// </summary>
+		/// <param name="rayHit"> The result of the raycast. </param>
+		/// <param name="moveDirection"> The resulting vector of the player's movement. </param>
+		void GetGroundInformation(RaycastHit2D rayHit, out Vector2 moveDirection)
 		{
-			(moveDirection = Quaternion.Euler(0, 0, 90) * rayHit.normal).Normalize();
-			moveDirection.Set(Mathf.Abs(moveDirection.x), Mathf.Abs(moveDirection.y));
+			float slopeAngle = Vector2.Angle(Vector2.up, rayHit.normal);
+			// Debug.DrawRay(this.transform.position, rayHit.normal, Color.green, 2f);
+					
+			if(slopeAngle <= steepSlopeAngle)
+			{
+				// Debug.Log(slopeAngle);
+				(moveDirection = Quaternion.Euler(0, 0, 270f) * rayHit.normal).Normalize();
+				//moveDirection.Set(Mathf.Abs(moveDirection.x), Mathf.Abs(moveDirection.y));
+			}
+			else
+				moveDirection = Vector2.right;
 		}
-		else
-			moveDirection = Vector2.right;
+
+		/// <summary>
+		/// 	In this class, the Update is being used to raycast the groundcheck and to get the vector in which possible player movement needs to be at.
+		/// </summary>
+		void Update()
+		{
+			rayHit = Physics2D.Raycast(transform.position + Vector3.down * raycastOffset, Vector2.down,
+			(groundRayDist + safetyGroundThreshold - raycastOffset), LayerMask.GetMask("Ground"));
+			Debug.DrawRay(transform.position + Vector3.down * raycastOffset, Vector2.down * (groundRayDist + safetyGroundThreshold - raycastOffset), Color.green);
+
+			GetGroundInformation(rayHit, out moveDirection);
+			// Debug.DrawRay(this.transform.position, moveDirection, Color.blue, 2f);
+			
+		}
+
+		/// <summary>
+		/// 	In this class, the FixedUpdate is being used to switch out of movement states and to process the player's movement, while snapping the player to the floor.
+		/// </summary>
+		void FixedUpdate()
+		{
+			float HorizontalAxis = Input.GetAxisRaw("Horizontal");
+
+			//	Checks if the player is grounded, if not, changes to the falling state
+			if(!rayHit)
+			{
+				animator.SetBool("IsFalling", true);
+				fsm.Transition<JumpFall>();
+			}
+
+			//	Changes state if the player Jumps
+			if(Input.GetKey(KeyCode.Space))
+			{
+				animator.SetBool("IsRising", true);
+				rBody.velocity = new Vector2(rBody.velocity.x, 0f);
+				fsm.Transition<JumpRise>();
+				return;
+			}
+
+			if(Input.GetKey(KeyCode.J))
+			{
+				animator.SetBool("IsAttacking", true);
+				animator.SetBool("IsWalking", false);
+				rBody.velocity = Vector2.zero;
+				fsm.Transition<Attack>();
+				return;
+			}
+
+			if(Input.GetKey(KeyCode.S))
+			{
+				animator.SetBool("IsCrouching", true);
+				animator.SetBool("IsWalking", false);
+				rBody.velocity = Vector2.zero;
+				fsm.Transition<Crouch>();
+				return;
+			}
+
+			// Debug.Log(rayHit.distance);
+
+			//	Ground Correction: Corrects the player's Y component when he's sunk into the ground or isn't close enough to it, but still considered grounded
+			if(rayHit.distance > ((groundRayDist - raycastOffset) - safetyGroundThreshold) &&
+			rayHit.distance < ((groundRayDist - raycastOffset) + safetyGroundThreshold))
+				this.transform.position += ((Vector3)rayHit.normal * ((groundRayDist - raycastOffset) - rayHit.distance));
+
+			//	Controls normal movement and sprite flipping
+			if(HorizontalAxis == 0f)
+			{
+				rBody.velocity = Vector2.zero;
+				fsm.Transition<Idle>();
+				animator.SetBool("IsWalking", false);
+			}
+			else if(HorizontalAxis > 0f)
+			{
+				sRenderer.flipX = false;
+				rBody.velocity = moveDirection * baseSpeed;
+			}
+			else
+			{
+				sRenderer.flipX = true;
+				rBody.velocity = Quaternion.Euler(0, 0, 180f) * moveDirection * baseSpeed;
+			}
+
+		}
+
+		public override void Exit() {	}
 	}
-
-	/// <summary>
-	/// 	In this class, the Update is being used to raycast the groundcheck and to get the vector in which possible player movement needs to be at.
-	/// </summary>
-	void Update()
-	{
-		rayHit = Physics2D.Raycast(transform.position, Vector2.down, (groundRayDist + safetyGroundThreshold), LayerMask.GetMask("Ground"));
-		Debug.DrawRay(transform.position, Vector2.down * (groundRayDist + safetyGroundThreshold), Color.green);
-
-		GetGroundInformation(rayHit, out moveDirection);
-	}
-
-	/// <summary>
-	/// 	In this class, the FixedUpdate is being used to switch out of movement states and to process the player's movement, while snapping the player to the floor.
-	/// </summary>
-	void FixedUpdate()
-	{
-		float HorizontalAxis = Input.GetAxisRaw("Horizontal");
-
-		//	Checks if the player is grounded, if not, changes to the falling state
-		if(!rayHit)
-		{
-			animator.SetBool("IsFalling", true);
-			fsm.Transition<JumpFall>();
-		}
-
-		//	Changes state if the player Jumps
-		if(Input.GetKey(KeyCode.Space))
-		{
-			animator.SetBool("IsRising", true);
-			rBody.velocity = new Vector2(rBody.velocity.x, 0f);
-			fsm.Transition<JumpRise>();
-			return;
-		}
-
-		//	Ground Correction: Corrects the player's Y component when he's sunk into the ground or isn't close enough to it, but still considered grounded
-		if(rayHit.distance > groundRayDist - safetyGroundThreshold && rayHit.distance < groundRayDist + safetyGroundThreshold)
-			this.transform.position += ((Vector3)rayHit.normal * (groundRayDist - rayHit.distance));
-
-		
-		//	Controls normal movement and sprite flipping
-		if(HorizontalAxis == 0f)
-		{
-			rBody.velocity = Vector2.zero;
-			fsm.Transition<Idle>();
-			animator.SetBool("IsWalking", false);
-		}
-		else if(HorizontalAxis > 0f)
-		{
-			sRenderer.flipX = false;
-			rBody.velocity = moveDirection * baseSpeed;
-		}
-		else
-		{
-			sRenderer.flipX = true;
-			rBody.velocity = moveDirection * -1 * baseSpeed;
-		}
-
-	}
-
-	public override void Exit() {	}
 }
