cd3756e3418252b2eec3eb3e20a4397108f87a2f
Gabriel <gdeperes@gmail.com>
2019-03-08 19:49:15 -0300

    New camera bound system, testing on Church-1

---
+++ Anathema/Assets/Scripts/Rooms/TilemapEditorDisable.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+namespace Anathema.Rooms
+{
+    [ExecuteInEditMode]
+    public class TilemapEditorDisable : MonoBehaviour
+    {
+        [SerializeField] private bool disableEditorRendering = true;
+        [SerializeField] private bool disableInGameRendering = true;
+        private TilemapRenderer[] renderers;
+        private void Awake()
+        {
+            renderers = GetComponentsInChildren<TilemapRenderer>(); 
+                
+            if (Application.isPlaying)
+            {
+                foreach (Transform child in transform)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                if (disableEditorRendering)
+                {
+                    foreach (var renderer in renderers)
+                    {
+                        Destroy(renderer);
+                    }
+                }
+            }
+        }
+
+        private void Update()
+        {
+            if (!Application.isPlaying)
+            {
+                foreach (Transform child in transform)
+                {
+                    child.gameObject.SetActive(Selection.Contains(child.gameObject));
+                }
+            }
+        }
+    }
+}
