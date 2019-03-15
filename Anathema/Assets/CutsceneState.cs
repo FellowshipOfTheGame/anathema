af32669489068a04eb699b7d9cc4424874f6c05c
Abner Eduardo Silveira Santos <abner.santos@usp.br>
2019-03-15 19:30:42 -0300

    Added power up animations

---
+++ Anathema/Assets/CutsceneState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anathema.Player
+{
+	/// <summary>
+	/// 	This is a void, empty state to prevent input during cutscenes.
+	/// </summary>
+	public class CutsceneState : Anathema.Fsm.PlayerState
+	{
+		public override void Enter()
+		{
+			
+		}
+
+		public override void Exit()
+		{
+
+		}
+	}
+}
