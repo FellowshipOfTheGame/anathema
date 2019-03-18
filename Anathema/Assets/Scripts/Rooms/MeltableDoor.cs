1aeb95b9b8f191eff449df2593935eb9d045f908
Gabriel <gdeperes@gmail.com>
2019-03-18 13:34:44 -0300

    Finished meltable door, fixed a couple stairs in Library-1

--- Anathema/Assets/Scripts/Rooms/MeltableDoor.cs
+++ Anathema/Assets/Scripts/Rooms/MeltableDoor.cs
@@ -5,18 +5,52 @@ namespace Anathema.Rooms
 {
     public class MeltableDoor : MonoBehaviour
     {
+        [SerializeField] private Vector3 fireScale;
+        [SerializeField] private Vector2 fireOffset;
+        [SerializeField] private float firePlaybackSpeed;
+        [SerializeField] private bool destroyed;
         private SpriteBurn spriteBurn;
+        private Collider2D mainCollider2D;
         private void Start()
         {
             spriteBurn = GetComponent<SpriteBurn>();
+            mainCollider2D = GetComponent<Collider2D>();
+            
+            spriteBurn.OnBurnComplete += DisableCollision;
         }
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Fire fire = other.GetComponent<Fire>();
-            if (fire)
+            if (!destroyed)
             {
-                spriteBurn.Burn();
+                Fire fire = other.GetComponent<Fire>();
+
+                if (fire)
+                {
+                    Animator fireAnimator = other.GetComponent<Animator>();
+                    destroyed = true;
+                    fire.transform.localScale = fireScale;
+                    fire.transform.localPosition += (Vector3) fireOffset;
+
+                    fireAnimator.speed = firePlaybackSpeed;
+
+                    if (fire)
+                    {
+                        spriteBurn.Burn();
+                    }
+                }
+
             }
+
+        }
+
+        private void DisableCollision()
+        {
+            mainCollider2D.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            spriteBurn.OnBurnComplete -= DisableCollision;
         }
     }
 }
