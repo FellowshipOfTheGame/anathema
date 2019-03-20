775ac34133e978776c1239c2244e4d74438598bd
HenriqueCamillo <henriquecamillo@usp.br>
2019-03-19 21:55:41 -0300

    Hordes Scripts added

---
+++ Anathema/Assets/Scripts/Rooms/HordeEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HordeEnemy : MonoBehaviour {
+	[SerializeField] private HordeManager hordeManager; 
+	
+	void Awake() {
+		hordeManager.EnemyQuantity++;
+	}
+	void OnDestroy() {
+		hordeManager.EnemyQuantity--;
+	}
+}
---
+++ Anathema/Assets/Scripts/Rooms/HordeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Anathema.Player;
+
+public class HordeManager : MonoBehaviour {
+	public UnityEvent OnHordeDeath;
+	[SerializeField] private int enemyQuantity;
+	[SerializeField] GameObject keyPrefab;
+
+	/// <summary>
+	/// Calls event when there is no enemy reamining in the horde
+	/// </summary>
+	/// <returns></returns>
+	public int EnemyQuantity {
+		get { return enemyQuantity; }
+		set {
+			enemyQuantity = value;
+			if (enemyQuantity == 0) {
+				OnHordeDeath.Invoke();
+			}
+		}
+	}
+
+	public void SpawnKey() {
+
+	}
+
+	public void ObtainFireAttack() {
+		GameObject player = PlayerFinder.Find("Player");
+		player.GetComponent<CutsceneState>().StartCutscene(CutsceneState.UpgradeType.FireAttack , true);
+	}
+}
