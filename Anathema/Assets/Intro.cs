80e5cea3479a9bb515c17785cad21ddfbc47563f
Abner Santos <abner.santos@usp.br>
2019-03-22 17:40:31 -0300

    Started implementing intro

---
+++ Anathema/Assets/Intro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+	[SerializeField] private Anathema.Dialogue.Dialogue introDialogue;
+	private void Awake()
+	{
+		Anathema.Dialogue.DialogueHandler.instance.StartDialogue(introDialogue);
+		Anathema.Dialogue.DialogueHandler.instance.OnDialogueEnd += EndIntro;
+	}
+
+	public void EndIntro()
+	{
+		GetComponent<Anathema.UI.CutsceneUnload>().LoadScene();
+	}
+
+
+}
