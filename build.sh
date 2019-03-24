39beb027be52e1a54ce9c8df5c1c6c99f4109a38
Gabriel de Moura <gdeperes@gmail.com>
2019-03-23 23:55:41 -0300

    - Fixed \r in Intro texts. - Broke some paragraphs into multiple dialogues. - Added build.sh to invoke CMake and CPack - Set WIX to the default generator in Windows64 platform

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e #Quit if any command fails
+
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cd $SCRIPTPATH
+
+if [ "$1" != "Windows64" ] && [ "$1" != "Linux64" ] && [ "$1" != "Mac64" ]; then
+    echo "build.sh: Invalid build type"
+    exit 1
+elif [ $1 = "clean"]; then
+    echo "build.sh: Removing build folder"
+    rm -rf build
+fi
+
+if [ ! -d build ]; then
+    mkdir build
+    cd build
+    echo "build.sh: Generating build files with CMake"
+    cmake -DUNITY_BUILD_TYPE=$1 ..
+else
+    echo "build.sh: Build folder already exists, assuming CMake has already generated buildfiles"
+    cd build
+fi
+
+echo "build.sh: Building project"
+cmake --build .
+echo "build.sh: Packaging project"
+cpack .
